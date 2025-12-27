d9d6d5b73e1f41ae82c928286760ccfd2c33ccc7
Habiba Shah <habibashah604@gmail.com>
2025-12-27 16:15:36 +0500

    change form name

--- WinFormsApp1/GameForm.cs
+++ WinFormsApp1/GameForm.cs
@@ -1,8 +1,8 @@
 namespace WinFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class GameForm : Form
     {
-        public Form1()
+        public GameForm()
         {
             InitializeComponent();
         }
